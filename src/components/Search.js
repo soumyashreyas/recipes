97b254fef43b6de51f6f30558b7f02168e8458f8
soumyashreyas <shreyas.soumya@gmail.com>
2022-06-06 14:57:22 +0800

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
+import { useSearchParams } from "react-router-dom";
 
 function Search() {
   const [search, setSearch] = useState("");
+  let [searchParams, setSearchParams] = useSearchParams();
+  let searchKey = searchParams.get("search");
+  console.log(searchKey + " " + search);
+
   const navigate = useNavigate();
   const handleOnClick = () => {
     let queryString = search;
-    queryString = queryString.replace(/ /g, '+');
-   
-    navigate({ pathname: "results/recipes/"+ `${queryString}` });
-    
+    queryString = queryString.replace(/ /g, "+");
+
+    navigate({
+      pathname: "results/recipes/",
+      search: "?search=" + `${queryString}`,
+    });
   };
   return (
-    <form className="form-inline my-2 my-lg-0" onSubmit={(e) => e.preventDefault()}>
+    <form
+      className="form-inline my-2 my-lg-0"
+      onSubmit={(e) => e.preventDefault()}
+    >
       <input
         className="form-control-search mr-sm-2"
         type="search"
