644342f16c0495209660c5212cdffbaf792b5dcd
soumyashreyas <shreyas.soumya@gmail.com>
2022-06-06 14:16:43 +0800

    Update Recipes.js

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -12,37 +12,36 @@ function Recipes() {
   const [recipes, setRecipe] = useState([]);
   const [hasMore, setHasMore] = useState(true);
   const [searchKey, setSearchKey] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   let { search } = useParams();
   if (search == null) {
     search = "fresh+picks";
   }
   let url = recipeListUrl(search);
-  
- 
+
   useEffect(() => {
     setRecipe([]);
     setSearchKey(search);
+    setIsLoading(true);
     fetchRecipes(true);
   }, [search]);
-
-
   const fetchRecipes = async (val) => {
     await apiAccessPoint
       .get(url)
       .then((response) => {
-        if(val){
+        if (val) {
           setRecipe([...response.data.hits]);
+          setIsLoading(false);
         } else {
           setRecipe([...recipes, ...response.data.hits]);
         }
-       
       })
       .catch((err) => {
         console.log("Error", err);
       });
   };
-  if (recipes.length>0) {
+  if (recipes.length > 0) {
     return (
       <InfiniteScroll
         dataLength={recipes.length}
@@ -54,11 +53,11 @@ function Recipes() {
         endMessage={<InfiniteScrollEnd />}
       >
         <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-          <RecipesView recipes={recipes} />
+          {isLoading ? <Loader /> : <RecipesView recipes={recipes} />}
         </div>
       </InfiniteScroll>
     );
-  } 
+  }
 }
 
 export default Recipes;
