2de471e822633b7b85ac5bcf9946b9794cc1112f
soumyashreyas <shreyas.soumya@gmail.com>
2022-06-06 15:19:40 +0800

    Create NotFound.js

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div className="d-flex justify-content-left m-lg-5">
+        <h2>No Page Found</h2>
+    </div>
+  )
+}
+
+export default NotFound
